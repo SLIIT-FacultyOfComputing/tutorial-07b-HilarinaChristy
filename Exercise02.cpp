5474cb259b9ead602a8d6170396ab5d691f12917
IT21127946 <86715390+HilarinaChristy@users.noreply.github.com>
2021-08-25 13:05:26 +0000

    Created and implemented dynamic objects.

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -15,14 +15,13 @@ int main() {
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
     Rectangle *r;// 1. Create a dynamic Rectangle type variable (pointer)
-    r = new Rectangle();// 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
- r -> setLength(length);
-  r -> setWidth(width);// 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+    r = new Rectangle(length,width);// 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+
    Circle *c;// 3. Create a dynamic Circle type variable (pointer)
-   c = new Circle();
- c ->setRadius(radius);// 4. Create a dynamic Circle Object set radius that was input from the keyboard
-   // 5. Call the display method of the Rectangle Object
-   // 6. Call the display method of the Circle Object
+   
+   c = new Circle(radius);// 4. Create a dynamic Circle Object set radius that was input from the keyboard
+   r ->display();// 5. Call the display method of the Rectangle Object
+   c ->display();// 6. Call the display method of the Circle Object
    delete r;// 7. Delete the Rectangle Object from memory
 
     delete c;// 8. Delete the Circle Object from memory
