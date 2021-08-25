0d639693900c3b6a478f24fa40a8bf20b2080672
IT21127946 <86715390+HilarinaChristy@users.noreply.github.com>
2021-08-25 12:43:39 +0000

    Implementation of constructors and destructors.

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -14,14 +14,18 @@ int main() {
    cin >> radius;
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
-   // 1. Create a dynamic Rectangle type variable (pointer)
-   // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
-   // 3. Create a dynamic Circle type variable (pointer)
-   // 4. Create a dynamic Circle Object set radius that was input from the keyboard
+    Rectangle *r;// 1. Create a dynamic Rectangle type variable (pointer)
+    r = new Rectangle();// 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+ r -> setLength(length);
+  r -> setWidth(width);// 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+   Circle *c;// 3. Create a dynamic Circle type variable (pointer)
+   c = new Circle();
+ c ->setRadius(radius);// 4. Create a dynamic Circle Object set radius that was input from the keyboard
    // 5. Call the display method of the Rectangle Object
    // 6. Call the display method of the Circle Object
-   // 7. Delete the Rectangle Object from memory
-   // 8. Delete the Circle Object from memory
+   delete r;// 7. Delete the Rectangle Object from memory
+
+    delete c;// 8. Delete the Circle Object from memory
   
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
--- Item.cpp
+++ Item.cpp
@@ -5,7 +5,19 @@ using namespace std;
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
-
+Item::Item(){
+   itemCode = 0;
+   unitPrice = 0;
+   discount = 0;
+}
+Item::Item(int code,float price){
+	itemCode = code;
+	unitPrice = price;
+	
+}
+Item::~Item(){
+	cout<<"Destructor called."<<endl;
+}
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
 }
@@ -22,3 +34,4 @@ void Item::display() {
   cout << "Item : " << itemCode << endl;
   cout << "Discounted Price " << discountedPrice() << endl;
 }
+
